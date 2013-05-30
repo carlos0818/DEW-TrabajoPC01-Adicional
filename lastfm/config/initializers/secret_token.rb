4cc3a4aaaaf65517824efb3a93eff18138dbf249
carlos <praimus666@gmail.com>
2013-05-30 11:54:54 -0500

    Iniciando el trabajo

---
+++ lastfm/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lastfm::Application.config.secret_token = 'f4263aeba547e5f1c71e13278cb7f6a6112bc29787f64b23807f25c6706d799bf40bd37136c176037d549a37ebb7ed70dbea3fc4f3e5737678b242e91a173479'
